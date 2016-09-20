6963ebcc345a537003724c3c0f95ddd515a2bc46
knadikari <knadikari@gmail.com>
2016-09-20 10:00:14 +0530

    added ip for xml

--- src/Check.java
+++ src/Check.java
@@ -2,6 +2,8 @@
 import java.io.File;
 import java.io.BufferedReader;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.transform.*;
 import javax.xml.transform.stream.*;
@@ -18,13 +20,13 @@ public class Check extends LanguageBaseListener{
         boolean result = false;
         BufferedReader reader = null;
         File file = new File("DNS.txt");
-        String text = null;
+        String[] text = null;
         try {
             reader = new BufferedReader(new FileReader(file));
 
 
-            while ((text = reader.readLine()) != null) {
-                if (text.equals(value)) {
+            while ((text = reader.readLine().split(" ")) != null) {
+                if (text[0].equals(value)) {
                     result = true;
                     break;
                 }
@@ -32,7 +34,7 @@ public class Check extends LanguageBaseListener{
 
             if (result == true) {
                 System.out.println("Method :" + value);
-                appendToXml(value);
+                appendToXml(text);
             } else {
                 System.out.println("Method Not Found");
                 System.exit(1);
@@ -43,17 +45,21 @@ public class Check extends LanguageBaseListener{
         }
     }
 
-    public void appendToXml(String methodName) throws Exception {
+    public void appendToXml(String[] methodDetails) throws Exception {
 
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
         Document document = documentBuilder.parse("server.xml");
         Element root = document.getDocumentElement();
 
-        Element newServer = document.createElement("servers");
+        Element newServer = document.createElement("methods");
+        Element newMethod = document.createElement("method");
         Element name = document.createElement("name");
-        name.appendChild(document.createTextNode(methodName));
-        root.appendChild(name);
+        Element ip = document.createElement("ip");
+        name.appendChild(document.createTextNode(methodDetails[0]));
+        ip.appendChild(document.createTextNode(methodDetails[1]));
+        newServer.appendChild(name);
+        newServer.appendChild(ip);
 
         DOMSource source = new DOMSource(document);
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
